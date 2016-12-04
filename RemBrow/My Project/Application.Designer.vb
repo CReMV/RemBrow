cad8ba988f9f200c5426a70240527ef6ce11929e
CReMV <chrisvasilas1993@yahoo.gr>
2016-12-04 23:38:01 +0200

    Creation

--- RemBrow/My Project/Application.Designer.vb
+++ RemBrow/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.RemBrow.Form1
+            Me.MainForm = Global.RemBrow.RemBrowMW
         End Sub
     End Class
 End Namespace
